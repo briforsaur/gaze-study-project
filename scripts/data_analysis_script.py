513dc0defdcb315e9ce742d8878ac325d96cedbd
Shane Forbrigger <66395421+briforsaur@users.noreply.github.com>
2024-10-16 13:32:53 -0300

    Move plotting function into its own module

--- scripts/data_analysis_script.py
+++ scripts/data_analysis_script.py
@@ -1,10 +1,9 @@
-import pupiltools.data_import as di
+import pupiltools.data_import as d_import
+import pupiltools.data_plotting as d_plot
 
 from argparse import ArgumentParser
 from dataclasses import dataclass
 from matplotlib import pyplot as plt
-from matplotlib.axes import Axes as mpl_axes
-import numpy as np
 from pathlib import Path
 
 
@@ -28,22 +27,6 @@ if __name__ == "__main__":
     eyes = (0, 1)
     file_path = args.data_path / f"{args.participant_id}.hdf5"
     hdf_path_info = {"group": "trials", "topic": "pupil", "method": "3d"}
-    participant_data = di.get_raw_participant_data(file_path, variables=variables, **hdf_path_info)
-    fig, axs = plt.subplots(2,1)
-    tasks = ("action", "observation")
-    plot_topics: dict[str, mpl_axes] = dict(zip(tasks, axs))
-    for title, ax in plot_topics.items():
-        ax.set_title(title.capitalize())
-        ax.set_xlabel('Time [s]')
-        ax.set_ylabel('Normalized Diameter [mm/mm]')
-        ax.set_ylim([0, 1.3])
-    eye = 0
-    for trial_data in participant_data:
-        t = trial_data["data"][eye]["timestamp"]
-        t = t - t[0]
-        d = trial_data["data"][eye]["diameter_3d"]
-        # Normalizing by the mean of the first second of data
-        d = d / np.mean(d[np.where(t<1)])
-        plot_topics[trial_data["attributes"]["task"]].plot(t, d, "k-")
-        fig.canvas.draw()
+    participant_data = d_import.get_raw_participant_data(file_path, variables=variables, **hdf_path_info)
+    fig = d_plot.plot_raw_pupil_diameter_comparison(participant_data)
     plt.show()
---
+++ src/pupiltools/data_plotting.py
@@ -0,0 +1,26 @@
+from matplotlib import pyplot as plt
+from matplotlib.axes import Axes as mpl_axes
+from matplotlib.figure import Figure as mpl_fig
+import numpy as np
+from .aliases import RawParticipantDataType
+
+
+def plot_raw_pupil_diameter_comparison(participant_data: RawParticipantDataType) -> mpl_fig:
+    fig, axs = plt.subplots(2,1)
+    fig.suptitle("Comparison of Relative Pupil Diameter Change from Baseline")
+    tasks = ("action", "observation")
+    plot_topics: dict[str, mpl_axes] = dict(zip(tasks, axs))
+    for title, ax in plot_topics.items():
+        ax.set_title(title.capitalize())
+        ax.set_xlabel('Time [s]')
+        ax.set_ylabel('Normalized Diameter [mm/mm]')
+        ax.set_ylim([0, 1.3])
+    eye = 0
+    for trial_data in participant_data:
+        t = trial_data["data"][eye]["timestamp"]
+        t = t - t[0]
+        d = trial_data["data"][eye]["diameter_3d"]
+        # Normalizing by the mean of the first second of data
+        d = d / np.mean(d[np.where(t<1)])
+        plot_topics[trial_data["attributes"]["task"]].plot(t, d)
+    return fig
