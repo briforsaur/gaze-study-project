902a4fbb3ad27688ccc6f7e3c8c09728a731aedd
Shane Forbrigger <66395421+briforsaur@users.noreply.github.com>
2024-10-16 13:59:17 -0300

    Normalize pupil diameter to zero and adjust fig size

--- src/pupiltools/data_plotting.py
+++ src/pupiltools/data_plotting.py
@@ -6,7 +6,7 @@ from .aliases import RawParticipantDataType
 
 
 def plot_raw_pupil_diameter_comparison(participant_data: RawParticipantDataType) -> mpl_fig:
-    fig, axs = plt.subplots(2,1)
+    fig, axs = plt.subplots(2,1, figsize=(10,10))
     fig.suptitle("Comparison of Relative Pupil Diameter Change from Baseline")
     tasks = ("action", "observation")
     plot_topics: dict[str, mpl_axes] = dict(zip(tasks, axs))
@@ -14,13 +14,13 @@ def plot_raw_pupil_diameter_comparison(participant_data: RawParticipantDataType)
         ax.set_title(title.capitalize())
         ax.set_xlabel('Time [s]')
         ax.set_ylabel('Normalized Diameter [mm/mm]')
-        ax.set_ylim([0, 1.3])
+        ax.set_ylim([-0.3, 0.3])
     eye = 0
     for trial_data in participant_data:
         t = trial_data["data"][eye]["timestamp"]
         t = t - t[0]
         d = trial_data["data"][eye]["diameter_3d"]
         # Normalizing by the mean of the first second of data
-        d = d / np.mean(d[np.where(t<1)])
+        d = d / np.mean(d[np.where(t<1)]) - 1
         plot_topics[trial_data["attributes"]["task"]].plot(t, d)
     return fig
