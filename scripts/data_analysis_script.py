8b6a1b9e807d97f115c3fc91c16d0c9cca6ac001
Shane Forbrigger <66395421+briforsaur@users.noreply.github.com>
2024-10-16 11:07:27 -0300

    Adjust participant data structure and plotting
    Change the participant data structure to more closely reflect the HDF file, which improves ease of plotting.

--- scripts/data_analysis_script.py
+++ scripts/data_analysis_script.py
@@ -3,6 +3,7 @@ import pupiltools.data_import as di
 from argparse import ArgumentParser
 from dataclasses import dataclass
 from matplotlib import pyplot as plt
+from matplotlib.axes import Axes as mpl_axes
 import numpy as np
 from pathlib import Path
 
@@ -27,29 +28,29 @@ if __name__ == "__main__":
     eyes = (0, 1)
     file_path = args.data_path / f"{args.participant_id}.hdf5"
     hdf_path_info = {"group": "trials", "topic": "pupil", "method": "3d"}
-    participant_data = {"action": [[], []], "observation": [[], []]}
+    participant_data = []
     with di.GazeDataFile(file_path, mode='r') as datafile:
         for i_trial in range(datafile.n_trials):
+            attr = datafile.get_attributes(trial=i_trial, **hdf_path_info)
+            participant_data.append({"attributes": attr, "data":[]})
             for eye in eyes:
                 data = datafile.get_data(trial=i_trial, eye=eye, variables=variables, **hdf_path_info)
-                attr = datafile.get_attributes(trial=i_trial, **hdf_path_info)
-                participant_data[attr['task']][eye].append(data)
+                participant_data[i_trial]["data"].append(data)
     fig, axs = plt.subplots(2,1)
-    axs[0].set_title("Action")
-    axs[0].set_xlabel('Time [s]')
-    axs[0].set_ylabel('Diameter [mm]')
-    axs[0].set_ylim([0, 1.3])
-    axs[1].set_title("Observation")
-    axs[1].set_xlabel('Time [s]')
-    axs[1].set_ylabel('Diameter [mm]')
-    axs[1].set_ylim([0, 1.3])
+    tasks = ("action", "observation")
+    plot_topics: dict[str, mpl_axes] = dict(zip(tasks, axs))
+    for title, ax in plot_topics.items():
+        ax.set_title(title.capitalize())
+        ax.set_xlabel('Time [s]')
+        ax.set_ylabel('Normalized Diameter [mm/mm]')
+        ax.set_ylim([0, 1.3])
     eye = 0
-    for axi, sub_data in enumerate(participant_data.values()):
-        for trial in range(60):
-            t = sub_data[eye][trial]["timestamp"]
-            t = t - t[0]
-            d = sub_data[eye][trial]["diameter_3d"]
-            d = d / np.mean(d[np.where(t<1)])
-            axs[axi].plot(t, d)
-            fig.canvas.draw()
+    for trial_data in participant_data:
+        t = trial_data["data"][eye]["timestamp"]
+        t = t - t[0]
+        d = trial_data["data"][eye]["diameter_3d"]
+        # Normalizing by the mean of the first second of data
+        d = d / np.mean(d[np.where(t<1)])
+        plot_topics[trial_data["attributes"]["task"]].plot(t, d, "k-")
+        fig.canvas.draw()
     plt.show()
