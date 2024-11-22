0494bde18e127c3ea23807f1c21a92077643cf33
Shane Forbrigger <66395421+briforsaur@users.noreply.github.com>
2024-11-21 20:16:06 -0400

    Add histograms of accuracy and F1 score

--- scripts/results_script.py
+++ scripts/results_script.py
@@ -2,6 +2,8 @@ from argparse import ArgumentParser
 from pathlib import Path
 import numpy as np
 import json
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 from sklearn.metrics import accuracy_score, f1_score
 
 def get_args():
@@ -27,12 +29,26 @@ def main(data_filepath, results_path):
             output = np.array(results["output"])
             print(phase)
             acc[phase][p_id] = accuracy_score(labels, output)
-            print(f"Accuracy:{accuracy_score(labels, output)}")
+            print(f"Accuracy:{acc[phase][p_id]:.3f}")
             f1[phase][p_id] = f1_score(labels, output)
-            print(f"F1      :{f1_score(labels, output)}")
-    print(f"Avg Accuracy: {np.mean(acc['test'])}")
-    print(f"Avg F1: {np.mean(f1['test'])}")
+            print(f"F1      :{f1[phase][p_id]:.3f}")
+    print(f"Avg Accuracy   : {np.mean(acc['test']):.3f}")
+    print(f"Median Accuracy: {np.median(acc['test']):.3f}")
+    fig_acc = plot_hist(acc["test"], xlabel="Accuracy", ylabel="Number of Model Instances")
+    fig_acc.savefig(results_path / "accuracy_histogram.pdf")
+    print(f"Avg F1         : {np.mean(f1['test']):.3f}")
+    print(f"Median F1      : {np.median(f1['test']):.3f}")
+    fig_f1 = plot_hist(f1["test"], xlabel="F1 Score", ylabel="Number of Model Instances")
+    fig_f1.savefig(results_path / "f1_histogram.pdf")
+    plt.show()
             
+
+def plot_hist(data: np.ndarray, xlabel: str, ylabel:str) -> Figure:
+    fig, ax = plt.subplots()
+    ax.hist(data)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    return fig
 
 
 
