d9063c12e33d8a5688759437ce924655da9b8bf1
Shane Forbrigger <66395421+briforsaur@users.noreply.github.com>
2024-11-26 15:33:02 -0400

    Minimal conference paper plotting script complete

--- scripts/conference_paper/plotting.py
+++ scripts/conference_paper/plotting.py
@@ -1,6 +1,11 @@
 from argparse import ArgumentParser
 import h5py
+import matplotlib.pyplot as plt
+import numpy as np
 from pathlib import Path
+
+import pupiltools.data_analysis as da
+import pupiltools.data_plotting as d_plt
 
 def get_args():
     parser = ArgumentParser()
@@ -11,13 +16,24 @@ def get_args():
 
 def main(data_filepath: Path):
     participant_id = "P23"
+    dt = 0.01
     tasks = ("action", "observation")
     variables = ("timestamp", "diameter_3d")
-    diameter = {}
+    diameter = dict.fromkeys(tasks)
+    t = dict.fromkeys(tasks)
+    trendlines = dict.fromkeys(tasks)
     with h5py.File(data_filepath, mode='r') as f_root:
         for task in tasks:
             dataset = f_root["/" + "/".join((participant_id, task))]
-            diameter.update({task: dataset.fields(variables)[:]})
+            data = dataset.fields(variables)
+            diameter[task] = np.concatenate((data["diameter_3d"][:,:,0], data["diameter_3d"][:,:,1]), axis=1)
+            trendlines[task] = da.get_trendlines_by_task(diameter[task])
+            t[task] = np.arange(start=0, stop=dt*diameter[task].shape[0], step=dt)
+    fig = plt.figure()
+    ax = fig.subplots()
+    for task in tasks:
+        d_plt.plot_trendline_range(ax, t[task], trendlines[task], xlabel="Time [s]", ylabel="Fractional Change in Pupil Diameter [mm/mm]", label=task)
+    plt.show()
 
 
 if __name__ == "__main__":
