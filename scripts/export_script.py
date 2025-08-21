d7bea195336b362492c9589891c419256fde1fcd
Shane Forbrigger <66395421+briforsaur@users.noreply.github.com>
2025-08-21 13:52:48 -0300

    Add docstring and adjust arguments
    The argument order of export_folder was not sensible given the interaction between the file type and the log files. Changed the order to read better in documentation. Required the arguments of the export script to be adjusted in name as well.

--- scripts/export_script.py
+++ scripts/export_script.py
@@ -4,57 +4,41 @@
 from argparse import ArgumentParser
 from pathlib import Path
 from pupiltools.export import export_folder
-from dataclasses import dataclass
 
 
-@dataclass
-class Args:
-    recording_path: Path
-    output_path: Path
-    log_file_path: Path
-    file_type: str
-    demographics_path: Path
-
-
-def get_args() -> Args:
+def get_args():
     parser = ArgumentParser()
     parser.add_argument(
-        "recording_path",
+        "folder_path",
         type=Path,
         help=(
             "Path to the the individual pupil recordings folders, i.e. "
-            "<recording_path>/000, <recording_path>/001, etc."
+            "<folder_path>/000, <folder_path>/001, etc."
         ),
     )
     parser.add_argument(
         "output_path", type=Path, help="Directory to save the exported data."
     )
     parser.add_argument(
-        "--log_file_path",
+        "--filetype",
+        default="csv",
+        help="Export file type. Can be either 'csv' or 'hdf'",
+    )
+    parser.add_argument(
+        "--experiment_log",
         type=Path,
         default=None,
         help="Log file from experiment describing which recordings to export. If not supplied, the entire folder of recordings will be exported.",
     )
     parser.add_argument(
-        "--file_type",
-        default="csv",
-        help="Export file type. Can be either 'csv' or 'hdf'",
-    )
-    parser.add_argument(
-        "--demographics_path",
+        "--demographics_log",
         type=Path,
         default=None,
         help="Log file from the experiment describing the participant demographics.",
     )
-    return Args(**vars(parser.parse_args()))
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = get_args()
-    export_folder(
-        args.recording_path,
-        args.output_path,
-        args.log_file_path,
-        args.file_type,
-        args.demographics_path,
-    )
+    export_folder(**vars(args))
