aa0572284828b36a93013a5f9fc74d5af19247f5
Shane Forbrigger <66395421+briforsaur@users.noreply.github.com>
2025-08-27 14:47:02 -0300

    Refactor into main function

--- scripts/resample_script.py
+++ scripts/resample_script.py
@@ -8,22 +8,11 @@ import pupiltools.export as pt_export
 from pupiltools.utilities import save_figure
 
 from argparse import ArgumentParser
-from dataclasses import dataclass
 from matplotlib import pyplot as plt
 from pathlib import Path
 
 
-@dataclass
-class Args:
-    participant_id: str
-    data_path: Path
-    fig_path: Path
-    T_resample: float
-    show_plot: bool
-    resampled_data_path: Path
-
-
-def get_args() -> Args:
+def get_args():
     parser = ArgumentParser()
     parser.add_argument("participant_id", help="ID code of participant (ex. 'P04').")
     parser.add_argument(
@@ -42,37 +31,40 @@ def get_args() -> Args:
         "--resampled_data_path", type=Path, default=None,
         help="Folder to save the resampled data. If not provided, data is not saved."
     )
-    args = parser.parse_args()
-    return Args(**vars(args))
+    return parser.parse_args()
 
 
-if __name__ == "__main__":
-    args = get_args()
+def main(participant_id: str, data_path: Path, T_resample: float, fig_path: Path | None = None, show_plot: bool = True, resampled_data_path: Path | None = None):
     variables = "all"
     eyes = (0, 1)
-    file_path = args.data_path / f"{args.participant_id}.hdf5"
+    file_path = data_path / f"{participant_id}.hdf5"
     hdf_path_info = {"group": "trials", "topic": "pupil", "method": "3d"}
     participant_data, participant_metadata = d_import.get_raw_participant_data(
         file_path, variables=variables, **hdf_path_info
     )
-    resampled_data = da.resample_data(participant_data, args.T_resample)
+    resampled_data = da.resample_data(participant_data, T_resample)
     resample_fig = d_plot.resample_comparison(
         participant_data[0]["data"][0],
         resampled_data[0]["data"][:,0],
         "diameter_3d",
         "Diameter [mm]"
     )
-    if args.resampled_data_path is not None:
+    if resampled_data_path is not None:
         complete_data_structure = {
             "attributes": participant_metadata,
             "data": resampled_data
         }
-        if not args.resampled_data_path.exists():
-            args.resampled_data_path.mkdir()
-        export_path = args.resampled_data_path / f"{args.participant_id}.hdf5"
+        if not resampled_data_path.exists():
+            resampled_data_path.mkdir()
+        export_path = resampled_data_path / f"{participant_id}.hdf5"
         pt_export.export_hdf(export_path, complete_data_structure)
-    if args.fig_path is not None:
-        figname = f"resample-comparison-{args.participant_id}-dt-{args.T_resample*1000:0.0f}ms"
-        save_figure(resample_fig, args.fig_path, figname, ("png", "svg"))
-    if args.show_plot:
+    if fig_path is not None:
+        figname = f"resample-comparison-{participant_id}-dt-{T_resample*1000:0.0f}ms"
+        save_figure(resample_fig, fig_path, figname, ("png", "svg"))
+    if show_plot:
         plt.show()
+
+
+if __name__ == "__main__":
+    args = get_args()
+    main(**vars(args))
