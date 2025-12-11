5cff29572e619958f9671d6c67674d2a18851d0e
Shane Forbrigger <66395421+briforsaur@users.noreply.github.com>
2025-12-11 10:22:21 -0400

    Add script to grab data from pldata files

---
+++ scripts/pldata_extraction.py
@@ -0,0 +1,43 @@
+from argparse import ArgumentParser, Namespace
+import json
+import msgpack as mpk
+from pathlib import Path
+from statistics import mean, pstdev
+
+
+def _get_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument("data_file_path", type=Path, help="Path to a PLDATA file.")
+    return parser.parse_args()
+
+
+def main(data_file_path: Path) -> None:
+    with open(data_file_path, "rb") as data_file:
+        unpacker = mpk.Unpacker(data_file, use_list=False)
+        msg_topic: str
+        msg_list = []
+        data_list = []
+        duration_list = []
+        n_msg = 0
+        for msg_topic, data_bytes in unpacker:
+            n_msg += 1
+            if msg_topic not in msg_list:
+                msg_list.append(msg_topic)
+            data = mpk.unpackb(data_bytes)
+            data_list.append(data)
+            if "surface" in msg_topic:
+                t0 = data["gaze_on_surfaces"][0]["timestamp"]
+                tf = data["gaze_on_surfaces"][-1]["timestamp"]
+                duration_list.append(tf - t0)
+    with open("./surface_data.json", "w") as f:
+        json.dump(data_list, f, indent=2)
+    assert float("NaN") not in duration_list
+    print(f"Message duration mean: {mean(duration_list)}")
+    print(f"Message duration std dev: {pstdev(duration_list)}")
+    print(msg_list)
+    print(f"Number of messages: {n_msg}")
+
+
+if __name__ == "__main__":
+    args = _get_args()
+    main(**vars(args))
